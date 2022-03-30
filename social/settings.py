bc95de4054ddb17f8fd799f03c6e12428d632bf0
jhamlal chelse <jhamlalchelse@gmail.com>
2022-03-30 16:54:50 +0530

    commit

--- social/settings.py
+++ social/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR  /  "staticfiles"
-STATIC_ROOT = os.path.join(BASE_DIR, 'reg/staticfiles')
+STATIC_ROOT = BASE_DIR  /  "social/staticfiles"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'reg/staticfiles')
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/myprofile/'
 MEDIA_ROOT =  BASE_DIR /'media'
 MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
