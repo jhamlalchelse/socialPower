e3e27f8c2d684886885b16674ea89f994fa880b2
jhamlal chelse <jhamlalchelse@gmail.com>
2022-03-30 20:53:32 +0530

    commit

--- social/settings.py
+++ social/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['socialpower.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['socialpower.herokuapp.com/myprofile/','localhost']
 # ALLOWED_HOSTS = []
 
 
